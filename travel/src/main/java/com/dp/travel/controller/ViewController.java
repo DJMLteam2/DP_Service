3cb3559b5b27e7606a336920d06b7b5eee12be9d
Felicette1963 <xorudlee4869@gmail.com>
2023-11-20 16:54:59 +0900

    JSONPARSER And Service.java Update

--- travel/src/main/java/com/dp/travel/controller/ViewController.java
+++ travel/src/main/java/com/dp/travel/controller/ViewController.java
@@ -1,18 +1,25 @@
 package com.dp.travel.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.client.RestTemplate;
 
+import com.dp.travel.data.dto.FastAPIAnswerDTO;
 import com.dp.travel.data.dto.QuestionForm;
+import com.dp.travel.service.SearchService;
 
 @Controller
 public class ViewController {
+
+    @Autowired
+    private SearchService searchService;
 
     @GetMapping("/view")
     public String index() {
@@ -20,26 +27,8 @@ public class ViewController {
     }
 
     @PostMapping("/create")
-    public String answer(QuestionForm form, Model model) {
-        // Spring에서 FastAPI로 데이터를 전송하는 부분
-        String apiUrl = "http://localhost:3000/getAnswer";
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        // QuestionForm을 JSON으로 변환
-        String requestBody = "{\"question\": \"" + form.getQuestion() + "\"}";
-
-        HttpEntity<String> request = new HttpEntity<>(requestBody, headers);
-
-        RestTemplate restTemplate = new RestTemplate();
-        String response = restTemplate.postForObject(apiUrl, request, String.class);
-
-        // FastAPI로부터의 응답 처리
-        System.out.println("Response from FastAPI: " + response);
-
-        model.addAttribute("response", response);
-
+    public String answer(QuestionForm questionForm, Model model) {
+        List<FastAPIAnswerDTO> fastAPIAnswerDTOs = searchService.SearchViewController(questionForm);
         return "redirect:/travel/view";  // 적절한 리다이렉션 처리
     }
 }

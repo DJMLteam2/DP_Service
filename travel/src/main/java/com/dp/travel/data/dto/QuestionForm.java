47defdcd76a48612e15abb32f1e1902a2482f406
Felicette1963 <xorudlee4869@gmail.com>
2023-11-20 12:30:22 +0900

    Update Dto / service / controller / index.html

---
+++ travel/src/main/java/com/dp/travel/data/dto/QuestionForm.java
@@ -0,0 +1,15 @@
+package com.dp.travel.data.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Data
+@AllArgsConstructor
+@Getter
+@Setter
+public class QuestionForm {
+    private String question;
+    private String area;
+}
--- travel/src/main/java/com/dp/travel/service/SearchService.java
+++ travel/src/main/java/com/dp/travel/service/SearchService.java
@@ -4,7 +4,12 @@ import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import com.dp.travel.data.dto.QuestionForm;
+
+import lombok.extern.slf4j.Slf4j;
+
 @Service
+@Slf4j
 public class SearchService {
 
     private final String FASTAPI_MODEL_URL = "http://your-fastapi-model-url"; // FastAPI 모델의 엔드포인트 URL
@@ -14,15 +19,27 @@ public class SearchService {
     public SearchService(RestTemplateBuilder restTemplateBuilder) {
         this.restTemplate = restTemplateBuilder.build();
     }
-
-    public String search(String searchTerms) {
+    public QuestionForm search(QuestionForm questionForm) {
         // RestTemplate를 사용하여 FastAPI 모델에 검색어를 전달하고 결과를 받아옴
-        String result = restTemplate.postForObject(FASTAPI_MODEL_URL, searchTerms, String.class);
+        log.info("service Quest: "+questionForm.getQuestion());
+        log.info("service Area: "+questionForm.getArea());
+        QuestionForm result = restTemplate.postForObject(FASTAPI_MODEL_URL, questionForm, QuestionForm.class);
 
         // 받아온 결과를 처리하거나 가공할 수 있음
 
         return result;
     }
+
+    // public String search(String Name) {
+    //     // RestTemplate를 사용하여 FastAPI 모델에 검색어를 전달하고 결과를 받아옴
+    //     String result = restTemplate.postForObject(FASTAPI_MODEL_URL, Name, String.class);
+
+    //     // 받아온 결과를 처리하거나 가공할 수 있음
+
+    //     return result;
+    // }
+
+
 
     // public String search(String searchTerms) {
     //     // FastAPI 모델에 검색어를 전달하고 결과를 받아옴

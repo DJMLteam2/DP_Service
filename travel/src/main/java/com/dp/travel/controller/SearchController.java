d97992fd485d33cb81369393c694b9d2fd2b1ed3
8trider <8trider@naver.com>
2023-11-20 09:11:57 +0900

    edited

--- travel/src/main/java/com/dp/travel/controller/SearchController.java
+++ travel/src/main/java/com/dp/travel/controller/SearchController.java
@@ -1,6 +1,14 @@
 package com.dp.travel.controller;
 
+import com.dp.travel.service.SearchService;
+import com.dp.travel.data.dto.SearchInfoDTO;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/search")

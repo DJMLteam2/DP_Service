c6b087e8ada4a26c8d887eccd5dba031eb7672e9
doxg <praze74@gmail.com>
2023-11-21 16:53:34 +0900

    view success

--- travel/src/main/java/com/dp/travel/controller/ViewController.java
+++ travel/src/main/java/com/dp/travel/controller/ViewController.java
@@ -1,16 +1,14 @@
 package com.dp.travel.controller;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.dp.travel.data.dto.FastAPIAnswerDTO;
 import com.dp.travel.data.dto.QuestionForm;
@@ -34,16 +32,23 @@ public class ViewController {
         return "travel/index";
     }
 
+
+
     @PostMapping("/create")
-    public String answer(QuestionForm questionForm, Model model) {
+    public String answer(QuestionForm questionForm, Model model, RedirectAttributes redirectAttributes) {
         List<FastAPIAnswerDTO> fastAPIAnswerDTOs = searchService.SearchViewController(questionForm);
         model.addAttribute("articles", fastAPIAnswerDTOs);
-        if(fastAPIAnswerDTOs.isEmpty()){
+
+        if (fastAPIAnswerDTOs.isEmpty()) {
             log.info("비었음");
-        }
-        else{
+        } else {
             log.info("차있음");
         }
-        return "travel/index";  // 적절한 리다이렉션 처리
+
+        // RedirectAttributes를 사용하여 Flash 속성 추가
+        redirectAttributes.addFlashAttribute("articles", fastAPIAnswerDTOs);
+
+        // 적절한 리다이렉션 처리
+        return "redirect:/view";
     }
 }
